58605a5164a9b3c15fb84463d1bb79aa059031ad
shashi <shashi.kamle99@gmail.com>
2024-01-29 10:40:46 +0530

    update changes

--- src/main/java/com/app/example/App.java
+++ src/main/java/com/app/example/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center>Welcome to DevOps CI/CD World Praveen..!!</center>";
+      return "<center>Hello World..!!</center>";
    }
 }
